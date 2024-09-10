e054dedc64bf0a6c9d2e2cc0f1e83166fcfd9c89
Ingrid Santana <is.matos@outlook.com>
2024-09-10 14:53:53 +0000

    Terceiro C

--- segundo.c
+++ segundo.c
@@ -1,7 +1,7 @@
 #include <stdio.h> // Biblioteca standard de Input/Output
 
 int main() {
-    char car;                                           //declara var. car do tipo char
+    char car;                                           // declara var. car do tipo char
     printf("Digite um caractere e ENTER\n");            // mostra na tela
     scanf("%c", &car);                                  // %c para ler ou mostrar char 1 char
     printf("Caractere digitado e' %c\n", car);
